f59d3b42051c9d84afd1e8280564367eba5cb0fe
prakhar gupta <chiru@pop-os.localdomain>
2020-08-06 01:52:14 +0530

    ok

--- instruction_comm.py
+++ instruction_comm.py
@@ -4,19 +4,7 @@ import os_lib
 
 # url_reg = \b((?:https?://)?(?:(?:www\.)?(?:[\da-z\.-]+)\.(?:[a-z]{2,6})|(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|(?:(?:[0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])))(?::[0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])?(?:/[\w\.-]*)*/?)\b
 
-# while True:
-# 	recognizer = sr.Recognizer()
-# 	with sr.Microphone() as source:
-# 		print('say something')
-# 		recognizer.adjust_for_ambient_noise(source)
-# 		audio = recognizer.listen(source)
-# 	try:
-# 		print('i think you said 		',recognizer.recognize_google(audio))
-# 		break
-# 	except sr.UnknownValueError:
-# 		print('not understand your command')
-# 	except sr.RequestError as e:
-# 		print('error {0}'.format(e))
+
 
 def tictok_game():
 	import tictok
@@ -29,16 +17,24 @@ def send_mail():
 def hi():
 	import greet_file
 
-hi()
 
-
-
-'''
-with sr.Microphone as source:
+while True:
 	recognizer = sr.Recognizer()
 	microphone = sr.Microphone()
-	recognizer.adjust_for_ambient_noise(source)
-	audio=recognizer.listen(source)
-	user=recognizer.recognize_google(audio)
-	print(user)
-'''
+	with microphone as source:
+		print('say something')
+		recognizer.adjust_for_ambient_noise(source)
+		audio = recognizer.listen(source)
+	try:
+		print('i think you said ',recognizer.recognize_google(audio))
+		user = recognizer.recognize_google(audio)
+		if user == 'hai':
+			hi()
+		elif user == 'bye':
+			quit()
+	except sr.UnknownValueError:
+		print('not understand your command')
+	except sr.RequestError as e:
+		print('error {0}'.format(e))
+
+
